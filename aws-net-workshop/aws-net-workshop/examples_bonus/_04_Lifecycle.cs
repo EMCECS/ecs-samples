61744b1f57b5b697ffea594436cfacb0e2568d85
Adrian Moreno <adrian@morenomartinez.com>
2017-05-17 15:31:33 +0200

    Update and add verbosity to Versioning and Lifecycle dotnet examples Add comments to object version sample
    Add comments to samples
    
    Revert name
    
    Update version of dependencies. Add app.config.
    
    Increase verbosity in Object Version and Lifecycle examples
    
    Update dotnet ListVersions example

--- aws-net-workshop/aws-net-workshop/examples_bonus/_04_Lifecycle.cs
+++ aws-net-workshop/aws-net-workshop/examples_bonus/_04_Lifecycle.cs
@@ -5,16 +5,40 @@ using System.Collections.Generic;
 
 namespace aws_net_workshop.examples_bonus
 {
+    /// <summary>
+    /// Configure bucket lifecycle
+    /// </summary>
+    /// <remarks>
+    /// Lifecycle configuration enables you to specify the lifecycle management
+    /// of objects in a bucket. The configuration is a set of one or more rules,
+    /// where each rule defines an action to apply to a group of objects.
+    /// The expiration action allows you to specify when the objects expire, then
+    /// ECS will delete the expired object.
+    ///
+    /// This sample will go through the following steps:
+    ///
+    ///   1. Create a bucket
+    ///   2. Configure bucket lifecycle rules to automatically set an expiration time
+    ///   3. Retrieve the bucket lifecycle configuration and verify the rules were applied
+    ///   4. Delete the bucket lifecycle configuration
+    ///   5. Retrieve the bucket lifecycle configuration and verify it is empty
+    ///   6. Delete the bucket
+    ///
+    /// </remarks>
     class _04_Lifecycle
     {
         public static void Main(string[] args)
         {
             // create the AWS S3 client
             AmazonS3Client s3 = AWSS3Factory.getS3Client();
+            
+            String bucketName = String.Join("-", AWSS3Factory.S3_BUCKET, DateTime.Now.ToString("yyyyMMddHHmmss"));
 
-            String bucketName = "bucket-" + DateTime.Now.ToString("yyyyMMddHHmmss");
+            //************************//
+            // 1. Create a bucket     //
+            //************************//
 
-            Console.Write(string.Format("Creating bucket '{0}'... ", bucketName));
+            Console.Write(string.Format(" [*] Creating bucket '{0}'... ", bucketName));
 
             PutBucketResponse pbRes = s3.PutBucket(bucketName);
             if (pbRes.HttpStatusCode != System.Net.HttpStatusCode.OK)
@@ -25,7 +49,11 @@ namespace aws_net_workshop.examples_bonus
             }
             Console.WriteLine("done");
 
-            Console.Write(string.Format("Updating lifecycle configuration for bucket '{0}'... ", bucketName));
+            //*************************************//
+            // 2. Configure bucket lifecycle rules //
+            //*************************************//
+
+            Console.Write(string.Format(" [*] Updating lifecycle configuration for bucket '{0}'... ", bucketName));
 
             LifecycleConfiguration newConfiguration = new LifecycleConfiguration
             {
@@ -35,14 +63,16 @@ namespace aws_net_workshop.examples_bonus
                     new LifecycleRule
                     {
                         Prefix = "Test-",
-                        Expiration = new LifecycleRuleExpiration { Days = 5 }
+                        Expiration = new LifecycleRuleExpiration { Days = 5 },
+                        Status = LifecycleRuleStatus.Enabled
                     },
                     // Rule to delete keys in subdirectory "Logs" after 2 days
                     new LifecycleRule
                     {
                         Prefix = "Logs/",
                         Expiration = new LifecycleRuleExpiration { Days = 2 },
-                        Id = "log-file-removal"
+                        Id = "log-file-removal",
+                        Status = LifecycleRuleStatus.Enabled
                     }
                 }
             };
@@ -63,7 +93,11 @@ namespace aws_net_workshop.examples_bonus
             }
             Console.WriteLine("done");
 
-            Console.Write(string.Format("Retrieving current lifecycle configuration for bucket '{0}'... ", bucketName));
+            //************************************************//
+            // 3. Retrieve the bucket lifecycle configuration //
+            //************************************************//
+
+            Console.Write(string.Format(" [*] Retrieving current lifecycle configuration for bucket '{0}'... ", bucketName));
 
             GetLifecycleConfigurationResponse glcRes = s3.GetLifecycleConfiguration(bucketName);
 
@@ -75,18 +109,21 @@ namespace aws_net_workshop.examples_bonus
             }
             Console.WriteLine("done");
 
-
-            Console.WriteLine(String.Format("-> Configuration contains {0} rules", glcRes.Configuration.Rules.Count));
+            Console.WriteLine(String.Format(" [x] Configuration contains {0} rules", glcRes.Configuration.Rules.Count));
             foreach (LifecycleRule rule in glcRes.Configuration.Rules)
             {
-                Console.WriteLine("->   Rule:");
-                Console.WriteLine("->     Prefix = " + rule.Prefix);
-                Console.WriteLine("->     Expiration (days) = " + rule.Expiration.Days);
-                Console.WriteLine("->     Id = " + rule.Id);
-                Console.WriteLine("->     Status = " + rule.Status);
+                Console.WriteLine(" [x]     Rule:");
+                Console.WriteLine(" [x]         Prefix = " + rule.Prefix);
+                Console.WriteLine(" [x]         Expiration (days) = " + rule.Expiration.Days);
+                Console.WriteLine(" [x]         Id = " + rule.Id);
+                Console.WriteLine(" [x]         Status = " + rule.Status);
             }
 
-            Console.Write(String.Format("Deleting current lifecycle configuration for bucket '{0}'... ", bucketName));
+            //**********************************************//
+            // 4. Delete the bucket lifecycle configuration //
+            //**********************************************//
+
+            Console.Write(String.Format(" [*] Deleting current lifecycle configuration for bucket '{0}'... ", bucketName));
 
             DeleteLifecycleConfigurationResponse dlcRes = s3.DeleteLifecycleConfiguration(bucketName);
 
@@ -99,7 +136,7 @@ namespace aws_net_workshop.examples_bonus
             Console.WriteLine("done");
 
 
-            Console.Write(String.Format("Verifying current lifecycle rules for bucket '{0}' are empty... ", bucketName));
+            Console.Write(String.Format(" [*] Verifying current lifecycle rules for bucket '{0}' are empty... ", bucketName));
 
             LifecycleConfiguration configuration = s3.GetLifecycleConfiguration(bucketName).Configuration;
 
@@ -111,7 +148,11 @@ namespace aws_net_workshop.examples_bonus
             }
             Console.WriteLine("done");
 
-            Console.Write(String.Format("Deleting bucket '{0}'... ", bucketName));
+            //************************//
+            // 5. Delete the bucket   //
+            //************************//
+
+            Console.Write(String.Format(" [*] Deleting bucket '{0}'... ", bucketName));
 
             DeleteBucketResponse dbRes = s3.DeleteBucket(bucketName);
 
@@ -123,7 +164,7 @@ namespace aws_net_workshop.examples_bonus
             }
             Console.WriteLine("done");
 
-
+            Console.WriteLine(" [*] Example is completed. Press any key to exit...");
             Console.ReadLine();
 
         }
