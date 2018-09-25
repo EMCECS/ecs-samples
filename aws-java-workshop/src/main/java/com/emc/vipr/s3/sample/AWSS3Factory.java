5a544e70f2c428700a6973993da405b2a37ffd4e
arnett, stu <stu.arnett@emc.com>
2018-09-25 11:14:48 -0500

    aws-java-workshop: simplified client construction

--- aws-java-workshop/src/main/java/com/emc/vipr/s3/sample/AWSS3Factory.java
+++ aws-java-workshop/src/main/java/com/emc/vipr/s3/sample/AWSS3Factory.java
@@ -14,14 +14,12 @@
  */
 package com.emc.vipr.s3.sample;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSStaticCredentialsProvider; //no 1.11-21
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.S3ClientOptions;
 import org.apache.commons.codec.binary.Base64;
 
 import java.security.KeyPair;
@@ -29,76 +27,48 @@ import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
 
 /**
- * Factory class to create the ViPR S3 client.  The client will be used in the examples for the
- * Java ViPR S3 interface.
+ * Factory class to create the AWS S3 client.  The client will be used in the examples for the
+ * AWS S3 interface.
  */
 public class AWSS3Factory {
-
-
-    public static final String S3_ENDPOINT = "http://x.x.x.x:9020";
-    public static final String S3_ACCESS_KEY_ID = "xxxxxx";
-    public static final String S3_SECRET_KEY = "xxxxxx";
-    public static final String S3_BUCKET = "workshop-bucket";
-
-    public static final String S3_VERSIONBUCKET = "awsversionbucket";
-
-
     /*
-     * The end point of the ViPR S3 REST interface - this should take the form of
+     * The end point of the S3 REST interface - this should take the form of
      * http://ecs-address:9020 or https://ecs-address:9021
      * or
      * https://object.ecstestdrive.com
-     * if you're using ECS Test Drive.  Note that you'll need to install the RSA root certificate in Java's
-     * cacerts file if you're ECS Test Drive:
-     * https://portal.ecstestdrive.com/Content/ECS%20Test%20Drive%20SSL%20issues%20with%20Java.docx
-     * or run the InstallCert program in the tools directory:
-     * java -jar installcert-usn-20140115.jar object.ecstestdrive.com:443
      */
-    //public static AmazonS3Client getS3Client() {
+    static final String S3_ENDPOINT = "https://object.ecstestdrive.com";
+
+    // the S3 access key id - this is equivalent to the user
+    static final String S3_ACCESS_KEY_ID = "xxxxxx";
+
+    // the S3 secret key associated with the S3_ACCESS_KEY_ID
+    static final String S3_SECRET_KEY = "xxxxxx";
+
+    // a unique bucket name to store objects
+    public static final String S3_BUCKET = "workshop-bucket";
+
+    public static final String S3_VERSIONBUCKET = "versioned-bucket";
+
+    // this should be a namespace-enabled baseURL w/ wildcard DNS & SSL
+    public static final String PUBLIC_ENDPOINT = "https://public.ecstestdrive.com";
+
     public static AmazonS3 getS3Client() {
-        BasicAWSCredentials creds = new BasicAWSCredentials(S3_ACCESS_KEY_ID, S3_SECRET_KEY);
+        AmazonS3ClientBuilder builder = AmazonS3Client.builder();
 
-  /*
-  //old v1.10 client
-        ClientConfiguration cc = new ClientConfiguration();
-        //cc.setProxyHost("localhost");
-        //cc.setProxyPort(8888);
-        // Force use of v2 Signer.
-        //cc.setSignerOverride("S3SignerType");
-		AmazonS3Client client = new AmazonS3Client(creds, cc);
-        client.setEndpoint(S3_ENDPOINT);
-*/
+        // set endpoint
+        builder.setEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(S3_ENDPOINT, "us-east-1"));
 
+        // set credentials
+        builder.setCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(S3_ACCESS_KEY_ID, S3_SECRET_KEY)));
 
-  // 1.11-100 good standard/basic with v4 auth doesn't work with 1.11-21 though
-        AwsClientBuilder.EndpointConfiguration ec = new AwsClientBuilder.EndpointConfiguration(S3_ENDPOINT,"us-east-1");
-        AmazonS3 client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(creds))
-                .withEndpointConfiguration(ec).build();
+        // switch to v2 auth
+        //builder.setClientConfiguration(new ClientConfiguration().withSignerOverride("S3SignerType"));
 
+        // path-style bucket naming is highly recommended
+        builder.setPathStyleAccessEnabled(true);
 
-/*
-// new client,but using v2 auth
-        ClientConfiguration cc = new ClientConfiguration();
-        cc.setSignerOverride("S3SignerType");
-
-        AmazonS3ClientBuilder.EndpointConfiguration ec = new AmazonS3ClientBuilder.EndpointConfiguration(S3_ENDPOINT,"us-east-1");
-        AmazonS3 client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(creds))
-                .withClientConfiguration(cc)
-                .withEndpointConfiguration(ec).withPathStyleAccessEnabled(true)
-                .build();
-*/
-
-
-
-
-/*
-//new client but deprecated way of using path style bucket naming
-        S3ClientOptions opts = new S3ClientOptions();
-        opts.setPathStyleAccess(true);
-        client.setS3ClientOptions(opts);
-  */
-
-		return client;
+        return builder.build();
     }
 
     // Generates a RSA key pair for testing.
