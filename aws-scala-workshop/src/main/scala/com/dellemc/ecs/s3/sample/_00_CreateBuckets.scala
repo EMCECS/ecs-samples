dbd3e65ba8f99337593223ad83a251e92f776328
seibed <david.seibert@emc.com>
2018-12-03 10:52:52 -0500

    Working scala project stubbed through bucket creation.

---
+++ aws-scala-workshop/src/main/scala/com/dellemc/ecs/s3/sample/_00_CreateBuckets.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Dell Inc. or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.dellemc.ecs.s3.sample
+
+import com.amazonaws.services.s3.AmazonS3
+
+object _00_CreateBuckets extends BucketAndObjectValidator {
+
+    def main(args: Array[String]): Unit = {
+        createBucket(AWSS3Factory.getS3ClientWithV2Signatures(), AWSS3Factory.S3_BUCKET)
+        createBucket(AWSS3Factory.getS3ClientWithV4Signatures(), AWSS3Factory.S3_BUCKET_2)
+    }
+
+    def createBucket(s3Client: AmazonS3, bucketName: String) = {
+        try {
+            checkBucketExistence(s3Client, bucketName)
+            // create the bucket - used for subsequent demo operations
+            s3Client.createBucket(bucketName)
+
+            checkBucketExistence(s3Client, bucketName)
+        } catch { case e: Exception => outputException(e) }
+    }
+
+}
